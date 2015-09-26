d973413a313cb8e354bd14add06d320876b6fec8
zhang881 <zhang881@purdue.edu>
2015-09-26 17:23:35 -0400

    project setup

---
+++ RoadTripPlanner/RoadTripPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoadTripPlanner
+//
+//  Created by Cici Zhang on 9/26/15.
+//  Copyright © 2015 Cici Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
